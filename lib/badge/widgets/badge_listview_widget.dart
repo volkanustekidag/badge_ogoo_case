155bf97786a1e6b26843419bdc6896f0176ae317
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-03-02 13:34:00 +0300

    Create badge listview widget

---
+++ lib/badge/widgets/badge_listview_widget.dart
@@ -0,0 +1,42 @@
+import 'package:badge_case/badge/view_model/badge_view_model.dart';
+import 'package:badge_case/badge/widgets/badge_list_item_widget.dart';
+import 'package:badge_case/badge/widgets/loading_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BagdeListViewWidget extends StatefulWidget {
+  final List<dynamic>? badgeListItem;
+
+  BagdeListViewWidget({
+    super.key,
+    this.badgeListItem,
+  });
+
+  @override
+  State<BagdeListViewWidget> createState() =>
+      _BagdeListViewWidgetState(badgeListItem: badgeListItem);
+}
+
+class _BagdeListViewWidgetState extends State<BagdeListViewWidget> {
+  final badgeManagerViewModel = Get.put(BadgeViewModel());
+  final List<dynamic>? badgeListItem;
+
+  _BagdeListViewWidgetState({
+    this.badgeListItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => badgeManagerViewModel.isLoadingListListview.value
+          ? ListView.builder(
+              itemCount: widget.badgeListItem!.length,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) => BadgeListItemWidget(
+                    bagdeListItem: widget.badgeListItem![index],
+                  ))
+          : const LoadingWidget(),
+    );
+  }
+}
