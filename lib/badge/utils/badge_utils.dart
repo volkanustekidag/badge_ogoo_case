1cbd917562e8b0c5f5a26298ee06edc585cd31d8
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-03-02 19:25:23 +0300

    Update badge utils

--- lib/badge/utils/badge_utils.dart
+++ lib/badge/utils/badge_utils.dart
@@ -8,10 +8,8 @@ class BadgeUtils {
   static getGridViewCouht(int l1, int index) =>
       (index * 4 < l1 && l1 - (index * 4) > 4) ? 4 : (l1 - (index * 4));
 
-  static formatDate(String date) {
-    print(DateFormat("MMMM d, HH:mm", 'tr').format(DateTime.parse(date)));
-    return DateFormat("MMMM d, HH:mm", 'tr').format(DateTime.parse(date));
-  }
+  static formatDate(String date) =>
+      DateFormat("MMMM d, HH:mm", 'tr').format(DateTime.parse(date));
 
   static getCardImagePathById(id) => "assets/image/$id.png";
 }
