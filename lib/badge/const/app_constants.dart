7349fa9695ec8f60bf37dc3b2b886dd526f93d12
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-03-02 12:35:51 +0300

    Create app constants file

---
+++ lib/badge/const/app_constants.dart
@@ -0,0 +1,16 @@
+class AppConstants {
+  //fonts
+  static const gothamBlack = "Gotham-Black";
+  static const gothamBold = "Gotham-Bold";
+  static const gothamBook = "Gotham-Book";
+  static const gothamLight = "Gotham-Light";
+  static const gothamMedium = "Gotham-Medium";
+
+  //svg path
+  static const flagIconSvgPath = "assets/svg/icon_flag.svg";
+  static const starIconSvgPath = "assets/svg/star.svg";
+
+  //json path
+  static const badgeJsonPath = "assets/json/badge-data.json";
+  static const listDataJsonPath = "assets/json/list-data.json";
+}
