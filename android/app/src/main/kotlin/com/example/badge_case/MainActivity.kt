baaa20bd8ee707049d34dea982c69ef46639c290
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-03-02 09:49:45 +0300

    project initialize

---
+++ android/app/src/main/kotlin/com/example/badge_case/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.badge_case
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/badge/view/badge_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BadgeManager extends StatefulWidget {
+  const BadgeManager({super.key});
+
+  @override
+  State<BadgeManager> createState() => _BadgeManagerState();
+}
+
+class _BadgeManagerState extends State<BadgeManager> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
