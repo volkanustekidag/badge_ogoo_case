a074ebbe443b04b10480bd50ea812d7d85161c8e
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-03-02 13:13:55 +0300

    Create badge utils file

--- lib/badge/utils/badge_utils.dart
+++ lib/badge/utils/badge_utils.dart
@@ -1,1 +1,17 @@
-class BadgeUtils {}
+import 'package:intl/intl.dart';
+
+class BadgeUtils {
+  static getSliderPageCount(int length) => length / 4 > (length / 4).round()
+      ? (length / 4).round() + 1
+      : (length / 4).round();
+
+  static getGridViewCouht(int l1, int index) =>
+      (index * 4 < l1 && l1 - (index * 4) > 4) ? 4 : (l1 - (index * 4));
+
+  static formatDate(String date) {
+    print(DateFormat("MMMM d, HH:mm", 'tr').format(DateTime.parse(date)));
+    return DateFormat("MMMM d, HH:mm", 'tr').format(DateTime.parse(date));
+  }
+
+  static getCardImagePathById(id) => "assets/image/$id.png";
+}
