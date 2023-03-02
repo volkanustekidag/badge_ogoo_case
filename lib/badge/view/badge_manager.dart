fefbac55ce7ac283eb868903d014bb4e3a247931
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-03-02 13:35:44 +0300

    Update badge manager view file

--- lib/badge/view/badge_manager.dart
+++ lib/badge/view/badge_manager.dart
@@ -1,4 +1,9 @@
+import 'package:badge_case/badge/view_model/badge_view_model.dart';
+import 'package:badge_case/badge/widgets/app_bar_widget.dart';
+import 'package:badge_case/badge/widgets/badge_listview_widget.dart';
+import 'package:badge_case/badge/widgets/card_slider_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BadgeManager extends StatefulWidget {
   const BadgeManager({super.key});
@@ -8,8 +13,44 @@ class BadgeManager extends StatefulWidget {
 }
 
 class _BadgeManagerState extends State<BadgeManager> {
+  final badgeManagerViewModel = Get.put(BadgeViewModel());
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent) {
+        badgeManagerViewModel.loadPaginatioList();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const AppBarWidget(),
+      body: SingleChildScrollView(
+        controller: scrollController,
+        child: Column(
+          children: [
+            Obx(
+              () => CardSlider(
+                badges: badgeManagerViewModel.badges.value,
+                allAvarageRating: badgeManagerViewModel.allAvarageRating.value,
+                allBadgeCount: badgeManagerViewModel.allBadgeCount.value,
+              ),
+            ),
+            const SizedBox(height: 17),
+            Obx(
+              () => BagdeListViewWidget(
+                badgeListItem: badgeManagerViewModel.paginatioList.value,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
